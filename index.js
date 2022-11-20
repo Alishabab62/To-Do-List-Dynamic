53b28a139e84eee60c93c3dffa1b2382b328ea5c
AliShabab62 <Alishabab62@gmail.com>
2022-11-20 17:49:32 +0530

    P

--- index.js
+++ index.js
@@ -10,19 +10,24 @@ btn.addEventListener("click", () => {
 function add_item() {
   if(input.value!=""){
     var new_div = document.createElement("div");
-    new_div.id = "new-div";
+    new_div.className = "new-div";
     ul.appendChild(new_div);
     const new_li = document.createElement("li");
     new_div.appendChild(new_li);
     new_li.innerText = input.value;
     const new_btn = document.createElement("button");
     new_btn.innerText = "Delete";
-    new_btn.id = "btn";
+    new_btn.className = "btn";
     new_div.appendChild(new_btn);
   }
 }
 
-new_li.addEventListener('click',function(){
-  new_li.style.textDecoration="underline";
-  console.log(new_div); 
-})
+
+ul.addEventListener('click' ,deleteItem)
+
+function deleteItem(e){
+  if(e.target.classList.contains('btn')){
+      console.log(e);
+      e.target.parentNode.remove();
+  }
+}
